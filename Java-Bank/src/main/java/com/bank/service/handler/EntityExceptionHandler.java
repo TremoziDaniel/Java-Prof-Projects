eb5fb7bb8048f33d16dc77114720ead7a1a5776c
TremoziDaniel <daniilizmajlov@gmail.com>
2023-10-14 17:47:41 +0100

    exceptionsV0.1

--- Java-Bank/src/main/java/com/bank/service/handler/EntityExceptionHandler.java
+++ Java-Bank/src/main/java/com/bank/service/handler/EntityExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.bank.service.handler;
 
+import com.bank.domain.exception.CannotBeCreatedException;
+import com.bank.domain.exception.EntityNotAvailableException;
 import com.bank.domain.exception.ItemNotFoundException;
+import com.bank.domain.exception.NotEnoughFundsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,12 +17,25 @@ public class EntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseStatusException ItemNotFoundException(ItemNotFoundException exception,
-                                                HttpServletRequest request) {
-        return new ResponseStatusException(exception.getMessage(), HttpStatus.NOT_FOUND);
+                                                         HttpServletRequest request) {
+        return new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage(), exception);
     }
 
-//    @ExceptionHandler
-//    public ResponseStatusException EntityNotAvailableException() {
-//        return null;
-//    }
+    @ExceptionHandler
+    public ResponseStatusException CannotBeCreatedException(CannotBeCreatedException exception,
+                                                            HttpServletRequest request) {
+        return new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler
+    public ResponseStatusException EntityNotAvailableException(EntityNotAvailableException exception,
+                                                               HttpServletRequest request) {
+        return new ResponseStatusException(HttpStatus.UNAUTHORIZED, exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler
+    public ResponseStatusException NotEnoughFundsException(NotEnoughFundsException exception,
+                                                           HttpServletRequest request) {
+        return  new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage(), exception);
+    }
 }
